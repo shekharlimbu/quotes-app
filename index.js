79a1052c145e496ec85d4f5f20663f5ecb34bf73
shekhar <shekhar.limbu.edu@gmail.com>
2021-09-13 17:47:04 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname,'quotes.html'));
+});
+
